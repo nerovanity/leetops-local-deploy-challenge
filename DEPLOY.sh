9dcb03e80191f841b4d0b37f52a267f388527898
nerovanity <hamani.dami.imad@gmail.com>
2025-12-13 00:12:28 +0100

    adding Log deployments and .gitignore

--- DEPLOY.sh
+++ DEPLOY.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+
 set -euo pipefail
+###############################################################################
+# CONFIGURATION
+###############################################################################
 
 WEB_ROOT="/usr/share/nginx/html"
 LOCAL_FILE="index.html"
 TARGET_FILE="${WEB_ROOT}/index.html"
 WEB_SERVICE="nginx"
+LOG_FILE=./deploy.log
 
 ###############################################################################
 # HELPER FUNCTIONS
@@ -18,6 +23,14 @@ fail() {
   log "ERROR: $*"
   exit 1
 }
+
+end_log()
+{
+    set +x
+    exec 1>&3 2>&4
+    exec 3>&- 4>&-
+}
+
 
 ###############################################################################
 # PRECHECKS
@@ -51,23 +64,27 @@ deploy() {
     sudo -v
     check
     log "Starting deployment of '${LOCAL_FILE}' to '${TARGET_FILE}'"
-
+    exec 3>&1 4>&2
+    exec 1> ${LOG_FILE} 2>&1
+    set -x
     log "copying the local file to the target file '${LOCAL_FILE}' to '${TARGET_FILE}'"
     if ! sudo cp "${LOCAL_FILE}" "${TARGET_FILE}" > /dev/null; then
-        fail "failed to copy the local file '${LOCAL_FILE}' to the web root dir '${WEB_ROOT}'"
-        fi
+        end_log
+        fail "failed to copy the local file '${LOCAL_FILE}' to the web root dir '${WEB_ROOT}', check '${LOG_FILE}' for more details"
+    fi
     log "setting permissions for '${TARGET_FILE}'"
     if ! sudo chmod 644 "${TARGET_FILE}"; then
-        fail "failed to set permissions"
+        end_log
+        fail "failed to set permissions, check '${LOG_FILE}' for more details"
     fi
 
     log "restarting the web service '${WEB_SERVICE}'"
     if ! sudo systemctl restart "${WEB_SERVICE}" > /dev/null; then
-        fail "failed to restart the web service '${WEB_SERVICE}'"
+        end_log
+        fail "failed to restart the web service '${WEB_SERVICE}', check '${LOG_FILE}' for more details"
     fi
-
+    end_log
     log "deployment finished successfully check http://localhost/"
 }
 
 deploy
-
